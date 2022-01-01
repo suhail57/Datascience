e9fb659f8b662375e0c4e4648601d7da5b4ca6c8
suhail57 <suhailjatoi57@gmail.com>
2022-01-01 18:54:13 +0500

    Work complete

---
+++ Assignment 1/Datascience by Jawan Pakistan.py
@@ -0,0 +1,34 @@
+
+#TASK1
+
+#print("Twinkle, twinkle, little star,\n         How I wonder what you are!\n                Up above the world so high,\n                Like a diamond in the sky.\n Twinkle, twinkle, little star,\n          How I wonder what you are")
+
+#Task2
+        #Version 3.10
+
+#Task3
+##from datetime import datetime
+##now = datetime.today()
+##print(f"Date and time : {now}")
+
+#Task4
+##import math
+##print("Enter the Radius of the Circle")
+##Radius = int(input("Radius: "))
+##Area = (math.pi*(Radius*Radius))
+##print(f"Area of Circle is: {Area}")
+
+#Task5
+
+##fname = input("Enter your first name: ")
+##lname = input("Enter your last name: ")
+##lname = ''.join(reversed(lname))
+##fname = ''.join(reversed(fname))
+##print(f"{fname} {lname}")
+
+#Task6
+
+##num1 = int(input("Enter the first number: "))
+##num2 = int(input("Enter the second number: "))
+##num1 = int(num1+num2)
+##print(num1)
